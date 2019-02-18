bb5da054dc6263ab9ba14767908e573c7bbc0375
Moiz Mansoor Ali <moizmansooraliis@gmail.com>
2019-02-18 19:08:42 +0530

    Add basic code to connect front-end and back-end

--- src/python/app.py
+++ src/python/app.py
@@ -1,6 +1,17 @@
-from flask import Flask
-app = Flask("PCDRS")
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 
-@app.route("/")
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/get_json', methods=['POST'])
 def hello():
-    return "Hello World!"
+    some_json = request.get_json()
+    if 'hello' in some_json:
+        print(some_json['hello'])
+    else:
+        print('Damn You')
+    return jsonify(some_json)
+
+if __name__ == '__main__':
+    app.run(debug=True)
